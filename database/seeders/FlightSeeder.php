d204a80e44c92fcc553546d97499ef1846893cb7
Henry Marsch <marschiee@gmail.com>
2022-11-12 15:36:18 +0100

    implemented Flight model, migration and seeder

---
+++ database/seeders/FlightSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FlightSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('flights')->insert([
+            'id' => 'bacbb338-5d01-471b-b5ea-a483a043c57a',
+            'airline_iata' => 'FR',
+            'flight_number' => 1024,
+            'from_code' => 'OTP',
+            'to_code' => 'STN',
+            'departure_date_utc' => '2022-01-19 09:10:00',
+        ]);
+
+        DB::table('flights')->insert([
+            'id' => 'bacbb338-5d01-471b-b5ea-a483a043c57b',
+            'airline_iata' => 'SE',
+            'flight_number' => 2048,
+            'from_code' => 'OTP',
+            'to_code' => 'STN',
+            'departure_date_utc' => '2022-02-20 10:10:00',
+        ]);
+
+        DB::table('flights')->insert([
+            'id' => 'bacbb338-5d01-471b-b5ea-a483a043c57c',
+            'airline_iata' => 'DK',
+            'flight_number' => 4096,
+            'from_code' => 'OTP',
+            'to_code' => 'STN',
+            'departure_date_utc' => '2022-03-21 11:10:00',
+        ]);
+    }
+}
